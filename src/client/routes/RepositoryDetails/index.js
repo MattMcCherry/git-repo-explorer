ffe303877bd8e72bd914d15bcb56afc79557e4e0
MattMcCherry <matt.mccherry@gmail.com>
2020-10-30 11:15:33 +0000

    Implemented RepositoryDetails component.

--- src/client/routes/RepositoryDetails/index.js
+++ src/client/routes/RepositoryDetails/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
 
-const RepositoryDetails = () => (
-  <Box>
-    <span>Repo details</span>
-  </Box>
-);
+const RepositoryDetails = () => {
+  const { owner, repo } = useParams();
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  const getData = async () => {
+    if (isLoading || data) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const repoDetails = await fetch(`https://api.github.com/repo/${owner}/${repo}`).then(response => response.json());
+      setData(repoDetails);
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <Box>
+      {isLoading && <span>loading</span>}
+      {!isLoading && data && <span>{data.name}</span>}
+    </Box>
+  );
+};
 
 export default RepositoryDetails;
