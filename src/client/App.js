a5c2355a8687c0f8d730fd8a3a0de2627f5acb0c
MattMcCherry <matt.mccherry@gmail.com>
2020-10-30 11:15:09 +0000

    Refactor & style App & RepoSearch components.

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './app.css';
 import {
-  Box, CssBaseline
+  Box, CssBaseline, makeStyles
 } from '@material-ui/core';
 import {
   BrowserRouter as Router,
@@ -11,24 +11,40 @@ import {
 import RepositorySearch from './routes/RepositorySearch';
 import RepositoryDetails from './routes/RepositoryDetails';
 
-const App = () => (
-  <>
-    <CssBaseline />
-    <Router>
-      <Box>
-        <Box>
-          <Switch>
-            <Route exact path="/">
-              <RepositorySearch />
-            </Route>
-            <Route path="/repo">
-              <RepositoryDetails />
-            </Route>
-          </Switch>
-        </Box>
+const App = () => {
+  const styles = makeStyles(() => ({
+    pageContainer: {
+      minHeight: '100vh',
+      background:
+      'linear-gradient(90deg, rgba(23,106,243,1) 0%, rgba(0,156,239,1) 100%)',
+      padding: '1rem',
+    },
+    contentContainer: {
+      maxWidth: '1200px',
+      margin: '0 auto',
+    }
+  }))();
+  return (
+    <Box className={styles.pageContainer}>
+      <Box className={styles.contentContainer}>
+        <CssBaseline />
+        <Router>
+          <Box>
+            <Box>
+              <Switch>
+                <Route exact path="/">
+                  <RepositorySearch />
+                </Route>
+                <Route path="/repo/:owner/:repo">
+                  <RepositoryDetails />
+                </Route>
+              </Switch>
+            </Box>
+          </Box>
+        </Router>
       </Box>
-    </Router>
-  </>
-);
+    </Box>
+  );
+};
 
 export default App;
--- src/client/routes/RepositorySearch/index.js
+++ src/client/routes/RepositorySearch/index.js
@@ -1,26 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import {
-  Input, Button, Box, Link, Typography
+  Input, Button, Box, Typography, makeStyles, Grid
 } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const RepositorySearch = () => {
+  const styles = makeStyles(() => ({
+    input: {
+      marginRight: '1rem',
+    },
+    title: {
+      marginBottom: '1.5rem',
+    },
+    container: {
+      background: '#f0f0f0',
+      borderRadius: '10px',
+      padding: '1rem',
+    },
+    resultsContainer: {
+      marginTop: '1.5rem',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    resultContainer: {
+      margin: '0.25rem 0'
+    },
+  }))();
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
-  const getSearchResults = async () => {
+  const sendRequest = useCallback(async () => {
+    if (isLoading) {
+      return;
+    }
+    setLoading(true);
     try {
       const data = await fetch(`./api/search/repositories?searchTerm=${searchTerm}`).then(response => response.json());
       setResults(data.items);
     } catch (err) {
       console.log(err);
     }
-  };
+    setLoading(false);
+  }, [isLoading, results, searchTerm]);
+
 
   return (
-    <Box>
-      <Input autoFocus onKeyUp={event => setSearchTerm(event.target.value)}>Hello world!</Input>
-      <Button onClick={getSearchResults}>Search</Button>
-      {results > 0 && (results.map(result => (<Typography><Link to={{ pathname: '/repo', state: { result } }}>{result.name}</Link></Typography>)))}
+    <Box className={styles.container}>
+      <Grid container>
+        <Grid item xs={12} sm={9}>
+          <Typography variant="h3" className={styles.title}>Repo explorer</Typography>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Input className={styles.input} autoFocus onKeyUp={event => setSearchTerm(event.target.value)}>Hello world!</Input>
+          <Button onClick={sendRequest}>Search</Button>
+        </Grid>
+      </Grid>
+      <Box className={styles.resultsContainer}>
+        {isLoading && <span>Loading...</span>}
+        {!isLoading && results.length > 0 && (results.map(result => (<Box className={styles.resultContainer}><Typography key={result.id}><Link to={`/repo/${result.full_name}`}>{result.name}</Link></Typography></Box>)))}
+        {!isLoading && results.length === 0 && <span>No results to show</span>}
+      </Box>
     </Box>
   );
 };
